765c6d3530721423033c77a50b352c32d036dc84
IT20611088 <it20611088@my.sliit.lk>
2023-10-08 22:26:06 +0530

    test1

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,8 @@ String kSomeConstant = 'some constant';
 int kSeedColor = 0xFF39D98A;
 int kPrimaryColor = 0xFF39D98A;
 
+int black2 = 0xFF525252;
+
 // For the signup form and screen
 int signupScreenColor = 0xFF05A560;
 String headingFontFamily = 'DM Sans';
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter_app/constants.dart';
 import 'package:flutter_app/screens/appointments_screen.dart';
 import 'package:flutter_app/screens/home_screen.dart';
 import 'package:flutter_app/screens/payment_screen.dart';
 import 'package:flutter_app/screens/signup_screen.dart';
+import 'package:flutter_app/screens/login.dart';
+import 'package:flutter_app/widgets/login_form_widget.dart';
+import 'package:flutter_app/widgets/social_buttons_widget.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -11,27 +17,117 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        backgroundColor: Color(signupScreenColor),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => HomeScreen(),
-                ));
-              },
-              child: Text('Home')),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SignupScreen()));
-            },
-            child: Text('Signup'),
-          )
-        ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(35, 70, 24, 35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'DM Sans',
+                      fontSize: 33,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  
+                  // const SizedBox(height: 15), // Use SizedBox for spacing
+                  Row(
+                    children: [
+                      Text(
+                        'Don t you have an account?  ',
+                        style: TextStyle(
+                          color: Color(black2),
+                          fontFamily: 'DM Sans',
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Sign Up',
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const SignupScreen()));
+                            },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // Additional widgets added after the green color area
+            const Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(24, 26, 24, 28),
+                  child: LoginFormWidget(),
+                ),
+                Row(children: [
+                  Padding(padding: EdgeInsets.fromLTRB(35, 0, 0, 0)),
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                    child: Text(
+                      'Or Login using',
+                      style: TextStyle(
+                        color: Color(0xFF1C1C28),
+                        fontSize: 13,
+                        fontFamily: 'DM Sans',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                        letterSpacing: 0.25,
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Divider()),
+                  Padding(padding: EdgeInsets.fromLTRB(0, 0, 35, 0)),
+                ]),
+                Padding(padding: EdgeInsets.fromLTRB(0, 27, 0, 0)),
+                Center(
+                  child: SocialLoginButtons(),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 60))
+              ],
+            )
+
+            // Add more widgets as needed
+          ],
+        ),
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.min,
+        // children: [
+        //   ElevatedButton(
+        //       onPressed: () {
+        //         Navigator.of(context).push(MaterialPageRoute(
+        //           builder: (context) => HomeScreen(),
+        //         ));
+        //       },
+        //       child: Text('Home')),
+        //   ElevatedButton(
+        //     onPressed: () {
+        //       Navigator.of(context).push(
+        //           MaterialPageRoute(builder: (context) => SignupScreen()));
+        //     },
+        //     child: Text('Signup'),
+        //   )
+        // ],
       ),
     );
   }
